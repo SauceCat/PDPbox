128a35ef8d3c31e86f7cea95772b1b6b3fc2f484
SauceCat <jiangchun.lee@gmail.com>
2018-05-21 14:39:41 +0800

    update

--- docs/conf.py
+++ docs/conf.py
@@ -18,10 +18,9 @@
 
 import os
 import sys
-curr_path = os.path.dirname(os.path.abspath(os.path.expanduser(__file__)))
-libpath = os.path.join(curr_path, '../')
-sys.path.insert(0, libpath)
-sys.path.insert(0, curr_path)
+sys.path.insert(0, os.path.abspath(os.path.pardir))
+
+import pdpbox
 
 # -- General configuration ------------------------------------------------
 
