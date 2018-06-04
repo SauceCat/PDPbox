c05221bbcdb6769ada245cf86b4e3074ecfbbf89
SauceCat <jiangchun.lee@gmail.com>
2018-06-04 15:41:50 +0800

    pygments_style default

--- docs/conf.py
+++ docs/conf.py
@@ -18,6 +18,8 @@
 
 import os
 import sys
+import sphinx_rtd_theme
+
 sys.path.insert(0, os.path.abspath(os.path.pardir))
 
 # -- General configuration ------------------------------------------------
@@ -72,7 +74,7 @@ language = None
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 # The name of the Pygments (syntax highlighting) style to use.
-pygments_style = 'sphinx'
+pygments_style = 'default'
 
 # If true, `todo` and `todoList` produce output, else they produce nothing.
 todo_include_todos = False
@@ -94,7 +96,8 @@ html_theme = 'sphinx_rtd_theme'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+# html_static_path = ['_static']
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 
 # -- Options for HTMLHelp output ------------------------------------------
