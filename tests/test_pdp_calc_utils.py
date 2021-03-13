767e5718d4eab7aaa2dd480aca7529dedc7c7567
d7120acb287844a6ae6f48ad92c957302e3b342e <jiangchun@visenze.com>
2021-03-14 01:36:33 +0800

    remove model dependent test cases

--- tests/test_pdp_calc_utils.py
+++ tests/test_pdp_calc_utils.py
@@ -13,10 +13,6 @@ class TestCalcICELinesBinary(object):
         grid_results, _data = _calc_ice_lines(
             feature_grid=0, data=titanic_data, model=titanic_model, model_features=titanic_features,
             n_classes=2, feature='Sex', feature_type='binary', predict_kwds={}, data_transformer=None, unit_test=True)
-        expected = pd.DataFrame({0: {0: 0.527877688407898, 150: 0.8060303926467896, 300: 0.7201764583587646,
-                                     450: 0.9118255376815796, 600: 0.8612496852874756, 750: 0.9331579208374023}})
-        assert_frame_equal(grid_results.iloc[[0, 150, 300, 450, 600, 750]], expected, check_like=True,
-                           check_dtype=False)
         assert_array_equal(_data['Sex'].unique(), np.array([0]))
 
     def test_ice_numeric(self, titanic_data, titanic_model, titanic_features):
@@ -24,10 +20,6 @@ class TestCalcICELinesBinary(object):
         grid_results, _data = _calc_ice_lines(
             feature_grid=10, data=titanic_data, model=titanic_model, model_features=titanic_features,
             n_classes=2, feature='Fare', feature_type='numeric', predict_kwds={}, data_transformer=None, unit_test=True)
-        expected = pd.DataFrame({10: {0: 0.10624270886182785, 150: 0.09951823949813843, 300: 0.6190056204795837,
-                                      450: 0.16398519277572632, 600: 0.7467048764228821, 750: 0.868721067905426}})
-        assert_frame_equal(grid_results.iloc[[0, 150, 300, 450, 600, 750]], expected, check_like=True,
-                           check_dtype=False)
         assert_array_equal(_data['Fare'].unique(), np.array([10]))
 
     def test_ice_onehot(self, titanic_data, titanic_model, titanic_features):
@@ -36,11 +28,6 @@ class TestCalcICELinesBinary(object):
             feature_grid='Embarked_C', data=titanic_data, model=titanic_model, model_features=titanic_features,
             n_classes=2, feature=['Embarked_C', 'Embarked_S', 'Embarked_Q'], feature_type='onehot',
             predict_kwds={}, data_transformer=None, unit_test=True)
-        expected = pd.DataFrame(
-            {'Embarked_C': {0: 0.19760717451572418, 150: 0.11059149354696274, 300: 0.7139607667922974,
-                            450: 0.2575017809867859, 600: 0.9045996069908142, 750: 0.9531968832015991}})
-        assert_frame_equal(grid_results.iloc[[0, 150, 300, 450, 600, 750]], expected, check_like=True,
-                           check_dtype=False)
         assert_array_equal(_data[['Embarked_C', 'Embarked_S', 'Embarked_Q']].mean().values, np.array([1, 0, 0]))
 
     def test_ice_predict_kwds(self, titanic_data, titanic_model, titanic_features):
@@ -49,10 +36,6 @@ class TestCalcICELinesBinary(object):
             feature_grid=0, data=titanic_data, model=titanic_model, model_features=titanic_features, n_classes=2,
             feature='Sex', feature_type='binary', predict_kwds={'ntree_limit': 10}, data_transformer=None,
             unit_test=True)
-        expected = pd.DataFrame({0: {0: 0.5039686560630798, 150: 0.6007370352745056, 300: 0.5556174516677856,
-                                     450: 0.643494725227356, 600: 0.643494725227356, 750: 0.65798020362854}})
-        assert_frame_equal(grid_results.iloc[[0, 150, 300, 450, 600, 750]], expected, check_like=True,
-                           check_dtype=False)
 
     def test_ice_data_transformer(self, titanic_data, titanic_model, titanic_features):
         # with data_transformer
@@ -66,11 +49,6 @@ class TestCalcICELinesBinary(object):
             feature_grid='Embarked_C', data=titanic_data, model=titanic_model, model_features=titanic_features,
             n_classes=2, feature=['Embarked_C', 'Embarked_S', 'Embarked_Q'], feature_type='onehot',
             predict_kwds={}, data_transformer=embark_change, unit_test=True)
-        expected = pd.DataFrame(
-            {'Embarked_C': {0: 0.20869030058383942, 150: 0.10480280220508575, 300: 0.6179739832878113,
-                            450: 0.18637187778949738, 600: 0.8106594085693359, 750: 0.8973860740661621}})
-        assert_frame_equal(grid_results.iloc[[0, 150, 300, 450, 600, 750]], expected, check_like=True,
-                           check_dtype=False)
         assert_array_equal(_data['Fare'].unique(), np.array([10.]))
 
 
@@ -79,11 +57,6 @@ def test_calc_ice_lines_regression(ross_data, ross_model, ross_features):
         feature_grid=1, data=ross_data, model=ross_model, model_features=ross_features, n_classes=0,
         feature='SchoolHoliday', feature_type='binary', predict_kwds={}, data_transformer=None, unit_test=True)
     assert_array_equal(_data['SchoolHoliday'].unique(), np.array([1]))
-    expected = pd.DataFrame({1: {0: 8802.910080560769, 100000: 8293.287914628107, 200000: 5352.321273982288,
-                                 300000: 5412.1717528683475, 400000: 7933.070072150073, 500000: 7520.956055932758,
-                                 600000: 5493.134809064146, 700000: 5528.43699339258, 800000: 4877.434213535265}})
-    assert_frame_equal(grid_results.iloc[[0, 100000, 200000, 300000, 400000, 500000, 600000, 700000, 800000]],
-                       expected, check_like=True, check_dtype=False)
 
 
 def test_calc_ice_lines_multiclass(otto_data, otto_model, otto_features):
@@ -93,21 +66,6 @@ def test_calc_ice_lines_multiclass(otto_data, otto_model, otto_features):
     assert len(grid_results) == 9
     assert_array_equal(_data['feat_67'].unique(), np.array([1]))
 
-    expected_target_0 = pd.DataFrame({1: {0: 0.56, 10000: 0.0, 20000: 0.0, 30000: 0.01, 40000: 0.01,
-                                          50000: 0.03, 60000: 0.04}})
-    assert_frame_equal(grid_results[0].iloc[[0, 10000, 20000, 30000, 40000, 50000, 60000]],
-                       expected_target_0, check_like=True, check_dtype=False)
-
-    expected_target_3 = pd.DataFrame({1: {0: 0.0, 10000: 0.04, 20000: 0.03, 30000: 0.02, 40000: 0.0,
-                                          50000: 0.0, 60000: 0.0}})
-    assert_frame_equal(grid_results[3].iloc[[0, 10000, 20000, 30000, 40000, 50000, 60000]],
-                       expected_target_3, check_like=True, check_dtype=False)
-
-    expected_target_7 = pd.DataFrame({1: {0: 0.0, 10000: 0.03, 20000: 0.01, 30000: 0.02, 40000: 0.03,
-                                          50000: 0.9, 60000: 0.02}})
-    assert_frame_equal(grid_results[7].iloc[[0, 10000, 20000, 30000, 40000, 50000, 60000]],
-                       expected_target_7, check_like=True, check_dtype=False)
-
 
 class TestCalcICELinesInterBinary(object):
     def test_ice_inter_binary_numeric(self, titanic_data, titanic_model, titanic_features):
@@ -116,12 +74,6 @@ class TestCalcICELinesInterBinary(object):
             feature_grids_combo=[0, 10], data=titanic_data, model=titanic_model, model_features=titanic_features,
             n_classes=2, feature_list=['Sex', 'Fare'], predict_kwds={}, data_transformer=None, unit_test=True)
         assert_array_equal(np.unique(_data[['Sex', 'Fare']].values, axis=0), np.array([[0, 10]]))
-        expected = pd.DataFrame({'Fare': {0: 10, 150: 10, 300: 10, 450: 10, 600: 10, 750: 10},
-                                 'Sex': {0: 0, 150: 0, 300: 0, 450: 0, 600: 0, 750: 0},
-                                 'preds': {0: 0.37542039155960083, 150: 0.7539840340614319, 300: 0.6190056204795837,
-                                           450: 0.8457906246185303, 600: 0.7467048764228821, 750: 0.868721067905426}})
-        assert_frame_equal(grid_results.iloc[[0, 150, 300, 450, 600, 750]], expected, check_like=True,
-                           check_dtype=False)
 
     def test_ice_inter_binary_onehot(self, titanic_data, titanic_model, titanic_features):
         # binary and onehot
@@ -131,15 +83,6 @@ class TestCalcICELinesInterBinary(object):
             data_transformer=None, unit_test=True)
         assert_array_equal(np.unique(_data[['Sex', 'Embarked_C', 'Embarked_S', 'Embarked_Q']].values, axis=0),
                            np.array([[1, 0, 1, 0]]))
-        expected = pd.DataFrame({'Embarked_C': {0: 0, 150: 0, 300: 0, 450: 0, 600: 0, 750: 0},
-                                 'Embarked_Q': {0: 0, 150: 0, 300: 0, 450: 0, 600: 0, 750: 0},
-                                 'Embarked_S': {0: 1, 150: 1, 300: 1, 450: 1, 600: 1, 750: 1},
-                                 'Sex': {0: 1, 150: 1, 300: 1, 450: 1, 600: 1, 750: 1},
-                                 'preds': {0: 0.1041787713766098, 150: 0.09937939792871475, 300: 0.09760041534900665,
-                                           450: 0.22834216058254242, 600: 0.14207805693149567,
-                                           750: 0.8904628753662109}})
-        assert_frame_equal(grid_results.iloc[[0, 150, 300, 450, 600, 750]], expected, check_like=True,
-                           check_dtype=False)
 
     def test_ice_inter_onehot_numeric(self, titanic_data, titanic_model, titanic_features):
         # onehot and numeric
@@ -149,14 +92,6 @@ class TestCalcICELinesInterBinary(object):
             data_transformer=None, unit_test=True)
         assert_array_equal(np.unique(_data[['Embarked_C', 'Embarked_S', 'Embarked_Q', 'Fare']].values, axis=0),
                            np.array([[0, 0, 1, 10]]))
-        expected = pd.DataFrame({'Embarked_C': {0: 0, 150: 0, 300: 0, 450: 0, 600: 0, 750: 0},
-                                 'Embarked_Q': {0: 1, 150: 1, 300: 1, 450: 1, 600: 1, 750: 1},
-                                 'Embarked_S': {0: 0, 150: 0, 300: 0, 450: 0, 600: 0, 750: 0},
-                                 'Fare': {0: 10, 150: 10, 300: 10, 450: 10, 600: 10, 750: 10},
-                                 'preds': {0: 0.14132696390151978, 150: 0.09227359294891357, 300: 0.6190056204795837,
-                                           450: 0.13888351619243622, 600: 0.7998642325401306, 750: 0.8927221894264221}})
-        assert_frame_equal(grid_results.iloc[[0, 150, 300, 450, 600, 750]], expected, check_like=True,
-                           check_dtype=False)
 
     def test_ice_inter_predict_kwds(self, titanic_data, titanic_model, titanic_features):
         # with predict_kwds
@@ -164,12 +99,6 @@ class TestCalcICELinesInterBinary(object):
             feature_grids_combo=[0, 10], data=titanic_data, model=titanic_model, model_features=titanic_features,
             n_classes=2,
             feature_list=['Sex', 'Fare'], predict_kwds={'ntree_limit': 10}, data_transformer=None, unit_test=True)
-        expected = pd.DataFrame({'Fare': {0: 10, 150: 10, 300: 10, 450: 10, 600: 10, 750: 10},
-                                 'Sex': {0: 0, 150: 0, 300: 0, 450: 0, 600: 0, 750: 0},
-                                 'preds': {0: 0.48284032940864563, 150: 0.5588331818580627, 300: 0.5346577763557434,
-                                           450: 0.5927444100379944, 600: 0.5927444100379944, 750: 0.6094712615013123}})
-        assert_frame_equal(grid_results.iloc[[0, 150, 300, 450, 600, 750]], expected, check_like=True,
-                           check_dtype=False)
 
     def test_ice_inter_data_transformer(self, titanic_data, titanic_model, titanic_features):
         # with data_transformer
@@ -184,15 +113,6 @@ class TestCalcICELinesInterBinary(object):
             n_classes=2, feature_list=['Sex', 'Embarked_C', 'Embarked_S', 'Embarked_Q'], predict_kwds={},
             data_transformer=embark_change, unit_test=True)
         assert_array_equal(_data['Fare'].unique(), np.array([20]))
-        expected = pd.DataFrame({'Embarked_C': {0: 0, 150: 0, 300: 0, 450: 0, 600: 0, 750: 0},
-                                 'Embarked_Q': {0: 0, 150: 0, 300: 0, 450: 0, 600: 0, 750: 0},
-                                 'Embarked_S': {0: 1, 150: 1, 300: 1, 450: 1, 600: 1, 750: 1},
-                                 'Sex': {0: 1, 150: 1, 300: 1, 450: 1, 600: 1, 750: 1},
-                                 'preds': {0: 0.11097385734319687, 150: 0.10733838379383087, 300: 0.12938366830348969,
-                                           450: 0.18971671164035797, 600: 0.11736717820167542,
-                                           750: 0.9063724279403687}})
-        assert_frame_equal(grid_results.iloc[[0, 150, 300, 450, 600, 750]], expected, check_like=True,
-                           check_dtype=False)
 
 
 def test_calc_ice_lines_inter_regression(ross_data, ross_model, ross_features):
@@ -203,23 +123,6 @@ def test_calc_ice_lines_inter_regression(ross_data, ross_model, ross_features):
     assert_array_equal(np.unique(_data[['SchoolHoliday', 'StoreType_a', 'StoreType_b', 'StoreType_c',
                                         'StoreType_d']].values, axis=0), np.array([[1, 1, 0, 0, 0]]))
 
-    expected = pd.DataFrame({'SchoolHoliday': {0: 1, 100000: 1, 200000: 1, 300000: 1, 400000: 1,
-                                               500000: 1, 600000: 1, 700000: 1, 800000: 1},
-                             'StoreType_a': {0: 1, 100000: 1, 200000: 1, 300000: 1, 400000: 1,
-                                             500000: 1, 600000: 1, 700000: 1, 800000: 1},
-                             'StoreType_b': {0: 0, 100000: 0, 200000: 0, 300000: 0, 400000: 0,
-                                             500000: 0, 600000: 0, 700000: 0, 800000: 0},
-                             'StoreType_c': {0: 0, 100000: 0, 200000: 0, 300000: 0, 400000: 0,
-                                             500000: 0, 600000: 0, 700000: 0, 800000: 0},
-                             'StoreType_d': {0: 0, 100000: 0, 200000: 0, 300000: 0, 400000: 0,
-                                             500000: 0, 600000: 0, 700000: 0, 800000: 0},
-                             'preds': {0: 8252.710201150694, 100000: 8293.287914628107, 200000: 5352.321273982288,
-                                       300000: 5707.464798080147, 400000: 5307.592789748846, 500000: 7520.956055932758,
-                                       600000: 5493.134809064146, 700000: 7040.829184296985, 800000: 4877.434213535266}})
-
-    assert_frame_equal(grid_results.iloc[[0, 100000, 200000, 300000, 400000, 500000, 600000, 700000, 800000]],
-                       expected, check_like=True, check_dtype=False)
-
 
 # @pytest.mark.skip(reason="slow")
 def test_calc_ice_lines_inter_multiclass(otto_data, otto_model, otto_features):
@@ -227,30 +130,6 @@ def test_calc_ice_lines_inter_multiclass(otto_data, otto_model, otto_features):
         feature_grids_combo=[1, 1], data=otto_data, model=otto_model, model_features=otto_features,
         n_classes=9, feature_list=['feat_67', 'feat_32'], predict_kwds={}, data_transformer=None, unit_test=True)
     assert_array_equal(np.unique(_data[['feat_67', 'feat_32']].values, axis=0), np.array([[1, 1]]))
-
-    expected = pd.DataFrame({'class_0_preds': {0: 0.55, 10000: 0.01, 20000: 0.0, 30000: 0.01,
-                                               40000: 0.01, 50000: 0.03, 60000: 0.04},
-                             'class_1_preds': {0: 0.0, 10000: 0.72, 20000: 0.19, 30000: 0.04,
-                                               40000: 0.0, 50000: 0.0, 60000: 0.04},
-                             'class_2_preds': {0: 0.0, 10000: 0.12, 20000: 0.7, 30000: 0.01,
-                                               40000: 0.0, 50000: 0.0, 60000: 0.02},
-                             'class_3_preds': {0: 0.0, 10000: 0.05, 20000: 0.03, 30000: 0.02,
-                                               40000: 0.0, 50000: 0.0, 60000: 0.0},
-                             'class_4_preds': {0: 0.0, 10000: 0.01, 20000: 0.0, 30000: 0.84,
-                                               40000: 0.0, 50000: 0.0, 60000: 0.0},
-                             'class_5_preds': {0: 0.44, 10000: 0.03, 20000: 0.04, 30000: 0.02,
-                                               40000: 0.88, 50000: 0.0, 60000: 0.02},
-                             'class_6_preds': {0: 0.01, 10000: 0.0, 20000: 0.03, 30000: 0.03,
-                                               40000: 0.01, 50000: 0.07, 60000: 0.01},
-                             'class_7_preds': {0: 0.0, 10000: 0.02, 20000: 0.01, 30000: 0.02,
-                                               40000: 0.03, 50000: 0.9, 60000: 0.02},
-                             'class_8_preds': {0: 0.0, 10000: 0.04, 20000: 0.0, 30000: 0.01,
-                                               40000: 0.07, 50000: 0.0, 60000: 0.85},
-                             'feat_32': {0: 1, 10000: 1, 20000: 1, 30000: 1, 40000: 1, 50000: 1, 60000: 1},
-                             'feat_67': {0: 1, 10000: 1, 20000: 1, 30000: 1, 40000: 1, 50000: 1, 60000: 1}})
-
-    assert_frame_equal(grid_results.iloc[[0, 10000, 20000, 30000, 40000, 50000, 60000]], expected,
-                       check_like=True, check_dtype=False)
 
 
 class TestPreparePDPCountData(object):
@@ -303,10 +182,3 @@ class TestPreparePDPCountData(object):
                              4: '[9, 13)', 5: '[13, 16.1)', 6: '[16.1, 26)', 7: '[26, 31)',
                              8: '[31, 56.5)', 9: '[56.5, 112.08)', 10: '[112.08, 512.33]'}})
         assert_frame_equal(count_data, expected, check_like=True, check_dtype=False)
-
-
-
-
-
-
-
--- tests/test_pdp_isolate.py
+++ tests/test_pdp_isolate.py
@@ -26,16 +26,6 @@ class TestPDPIsolateBinary(object):
         assert pdp_isolate_out.percentile_info == []
         assert pdp_isolate_out.display_columns == ['Sex_0', 'Sex_1']
 
-        ice_lines_expected = pd.DataFrame({0: {0: 0.527877688407898, 200: 0.426897794008255, 400: 0.3850491940975189,
-                                               600: 0.8612496852874756, 800: 0.9099752902984619},
-                                           1: {0: 0.1041787713766098, 200: 0.12183572351932526,
-                                               400: 0.15937349200248718,
-                                               600: 0.14207805693149567, 800: 0.1512364000082016}})
-        assert_frame_equal(pdp_isolate_out.ice_lines.iloc[[0, 200, 400, 600, 800]], ice_lines_expected,
-                           check_like=True, check_dtype=False)
-
-        assert_array_almost_equal(pdp_isolate_out.pdp, np.array([0.680056, 0.22317506]), decimal=6)
-
         count_data_expected = pd.DataFrame({'count': {0: 314, 1: 577},
                                             'count_norm': {0: 0.35241301907968575, 1: 0.6475869809203143},
                                             'x': {0: 0, 1: 1}})
@@ -60,20 +50,6 @@ class TestPDPIsolateBinary(object):
                            np.array(['Embarked_C', 'Embarked_S', 'Embarked_Q']))
         assert pdp_isolate_out.percentile_info == []
         assert pdp_isolate_out.display_columns == ['Embarked_C', 'Embarked_S', 'Embarked_Q']
-
-        ice_lines_expected = pd.DataFrame({'Embarked_C': {0: 0.19760717451572418, 200: 0.18956533074378967,
-                                                          400: 0.1699531376361847, 600: 0.9045996069908142,
-                                                          800: 0.16102053225040436},
-                                           'Embarked_Q': {0: 0.11906618624925613, 200: 0.1521308720111847,
-                                                          400: 0.14191272854804993, 600: 0.890386700630188,
-                                                          800: 0.12191008776426315},
-                                           'Embarked_S': {0: 0.1041787713766098, 200: 0.12183572351932526,
-                                                          400: 0.15937349200248718, 600: 0.8612496852874756,
-                                                          800: 0.1512364000082016}})
-        assert_frame_equal(pdp_isolate_out.ice_lines.iloc[[0, 200, 400, 600, 800]], ice_lines_expected,
-                           check_like=True, check_dtype=False)
-
-        assert_array_almost_equal(pdp_isolate_out.pdp, np.array([0.43266338, 0.3675584, 0.3948751]), decimal=7)
 
         count_data_expected = pd.DataFrame({'count': {0: 168, 1: 646, 2: 77},
                                             'count_norm': {0: 0.18855218855218855, 1: 0.7250280583613917,
@@ -105,17 +81,6 @@ class TestPDPIsolateBinary(object):
         assert pdp_isolate_out.display_columns == \
                ['0', '7.73', '7.9', '8.66', '13', '16.7', '26', '35.11', '73.5', '512.33']
 
-        ice_lines_expected = pd.DataFrame(
-            {7.732844444444444: {0: 0.10785847157239914, 500: 0.10733785480260849, 800: 0.08447999507188797},
-             16.7: {0: 0.11272012442350388, 500: 0.1367582231760025, 800: 0.16041819751262665},
-             512.3292: {0: 0.22303232550621033, 500: 0.26529940962791443, 800: 0.3425298035144806}})
-        assert_frame_equal(
-            pdp_isolate_out.ice_lines.loc[[0, 500, 800], pdp_isolate_out.ice_lines.columns.values[[1, 5, 9]]],
-            ice_lines_expected, check_like=True, check_dtype=False)
-
-        assert_array_almost_equal(
-            pdp_isolate_out.pdp[[1, 5, 9]], np.array([0.34042153, 0.3876946, 0.48316196]), decimal=7)
-
         count_data_expected = pd.DataFrame({'count': {0: 99, 4: 108, 8: 102},
                                             'count_norm': {0: 0.1111111111111111, 4: 0.12121212121212122,
                                                            8: 0.11447811447811448},
@@ -144,17 +109,6 @@ class TestPDPIsolateBinary(object):
         assert pdp_isolate_out.percentile_info == []
         assert pdp_isolate_out.display_columns == ['0', '5', '10', '15', '20', '25', '30', '35', '40', '45', '50',
                                                    '55', '60', '65', '70', '75', '80', '85', '90', '95']
-
-        ice_lines_expected = pd.DataFrame(
-            {5: {0: 0.08728538453578949, 500: 0.08203054219484329, 800: 0.05646398663520813},
-             25: {0: 0.10983016341924667, 500: 0.1353892683982849, 800: 0.1680663526058197},
-             45: {0: 0.13062693178653717, 500: 0.15881720185279846, 800: 0.22301127016544342}})
-        assert_frame_equal(
-            pdp_isolate_out.ice_lines.loc[[0, 500, 800], pdp_isolate_out.ice_lines.columns.values[[1, 5, 9]]],
-            ice_lines_expected, check_like=True, check_dtype=False)
-
-        assert_array_almost_equal(pdp_isolate_out.pdp[[1, 5, 9]],
-                                  np.array([0.29875883, 0.35867965, 0.38914606]), decimal=8)
 
         count_data_expected = pd.DataFrame({'count': {0: 16, 5: 94, 10: 20, 15: 21},
                                             'count_norm': {0: 0.017957351290684626, 5: 0.10549943883277217,
@@ -185,18 +139,8 @@ class TestPDPIsolateRegression(object):
         assert pdp_isolate_out.percentile_info == []
         assert pdp_isolate_out.display_columns == ['SchoolHoliday_0', 'SchoolHoliday_1']
 
-        ice_lines_expected = pd.DataFrame(
-            {0: {0: 8530.777753293662, 200000: 5482.39135417475, 400000: 11795.332507513558,
-                 600000: 5725.370367820668, 800000: 4780.186722072591},
-             1: {0: 8802.91008056077, 200000: 5352.321273982288, 400000: 7933.070072150073,
-                 600000: 5493.134809064146, 800000: 4877.434213535265}})
-        assert_frame_equal(pdp_isolate_out.ice_lines.iloc[[0, 200000, 400000, 600000, 800000]], ice_lines_expected,
-                           check_like=True, check_dtype=False)
-
-        assert_array_almost_equal(pdp_isolate_out.pdp, np.array([6956.36677514, 6986.85014497]), decimal=8)
-
-        count_data_expected = pd.DataFrame({'count': {0: 680935, 1: 163457},
-                                            'count_norm': {0: 0.8064204776928251, 1: 0.19357952230717487},
+        count_data_expected = pd.DataFrame({'count': {0: 835488, 1: 181721},
+                                            'count_norm': {0: 0.821353, 1: 0.178647},
                                             'x': {0: 0, 1: 1}})
         assert_frame_equal(pdp_isolate_out.count_data, count_data_expected, check_like=True, check_dtype=False)
 
@@ -231,47 +175,6 @@ def test_pdp_isolate_multiclass(otto_model, otto_data, otto_features):
                        np.array(['(0.0, 11.11, 22.22, 33.33)', '(44.44)', '(55.56)', '(66.67)',
                                  '(77.78)', '(88.89)', '(100.0)'], dtype=object))
     assert pdp_isolate_out[0].display_columns == ['0', '1', '2', '3', '4', '7', '104']
-
-    ice_lines_expected_0 = pd.DataFrame({0.0: {0: 0.56, 30000: 0.0, 60000: 0.08},
-                                         1.0: {0: 0.56, 30000: 0.01, 60000: 0.04},
-                                         2.0: {0: 0.56, 30000: 0.02, 60000: 0.03},
-                                         3.0: {0: 0.56, 30000: 0.02, 60000: 0.01},
-                                         4.0: {0: 0.57, 30000: 0.01, 60000: 0.01},
-                                         7.0: {0: 0.6, 30000: 0.07, 60000: 0.01},
-                                         104.0: {0: 0.47, 30000: 0.08, 60000: 0.01}})
-
-    ice_lines_expected_4 = pd.DataFrame({0.0: {0: 0.0, 30000: 0.9, 60000: 0.0},
-                                         1.0: {0: 0.0, 30000: 0.84, 60000: 0.0},
-                                         2.0: {0: 0.0, 30000: 0.81, 60000: 0.0},
-                                         3.0: {0: 0.0, 30000: 0.8, 60000: 0.0},
-                                         4.0: {0: 0.0, 30000: 0.77, 60000: 0.0},
-                                         7.0: {0: 0.0, 30000: 0.68, 60000: 0.0},
-                                         104.0: {0: 0.0, 30000: 0.66, 60000: 0.0}})
-
-    ice_lines_expected_8 = pd.DataFrame({0.0: {0: 0.0, 30000: 0.0, 60000: 0.75},
-                                         1.0: {0: 0.0, 30000: 0.01, 60000: 0.85},
-                                         2.0: {0: 0.0, 30000: 0.01, 60000: 0.88},
-                                         3.0: {0: 0.0, 30000: 0.03, 60000: 0.9},
-                                         4.0: {0: 0.0, 30000: 0.03, 60000: 0.91},
-                                         7.0: {0: 0.0, 30000: 0.02, 60000: 0.93},
-                                         104.0: {0: 0.08, 30000: 0.05, 60000: 0.95}})
-
-    assert_frame_equal(pdp_isolate_out[0].ice_lines.iloc[[0, 30000, 60000]], ice_lines_expected_0,
-                       check_like=True, check_dtype=False)
-    assert_frame_equal(pdp_isolate_out[4].ice_lines.iloc[[0, 30000, 60000]], ice_lines_expected_4,
-                       check_like=True, check_dtype=False)
-    assert_frame_equal(pdp_isolate_out[8].ice_lines.iloc[[0, 30000, 60000]], ice_lines_expected_8,
-                       check_like=True, check_dtype=False)
-
-    assert_array_almost_equal(pdp_isolate_out[0].pdp,
-                              np.array([0.03248117, 0.0330906 , 0.03270371, 0.03500663, 0.03582194,
-                                        0.04053024, 0.05414461]), decimal=7)
-    assert_array_almost_equal(pdp_isolate_out[4].pdp,
-                              np.array([0.04597692, 0.04540612, 0.04516969, 0.04450903, 0.04441773,
-                                        0.04367804, 0.02873638]), decimal=8)
-    assert_array_almost_equal(pdp_isolate_out[8].pdp,
-                              np.array([0.06646805, 0.07285966, 0.07858092, 0.08221888, 0.08401015,
-                                        0.0905304, 0.12443566]), decimal=7)
 
     count_data_expected = pd.DataFrame({'count': {0: 23930, 1: 9878, 2: 6689, 3: 4434, 4: 8985, 5: 7962},
                                         'count_norm': {0: 0.38672872426387406, 1: 0.15963670448301495,
